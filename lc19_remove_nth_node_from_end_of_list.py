1df4eb476b05930ab183381dae7b130e087dfde7
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2025-09-26 17:28:34 -0400

    feat: base challenge for lc19, remove Nth node from end of list

---
+++ lc19_remove_nth_node_from_end_of_list.py
@@ -0,0 +1,85 @@
+import re
+import json
+from typing import Optional
+import time
+
+# pylint:disable=C0103,R0903,W0613
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):  # pylint: disable=W0622
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        pass
+
+
+if __name__ == "__main__":
+
+    def get_listnode_as_str(node: ListNode) -> str:
+        """Get ListNode values as a string resembling a list"""
+        values = []
+        current = node
+        while current:
+            values.append(current.val)
+            current = current.next
+        output = json.dumps(values)
+        output = re.sub(r"[^\d, \[\]]+", "", output)
+        output.replace("None", "")
+        return output
+
+    def make_list(nodes: list) -> ListNode | None:
+        """Generate linked list based on list parameter"""
+        if not nodes:
+            return None
+        if nodes[0] == 0:
+            return ListNode(0, make_list(nodes[1:]))
+        return ListNode(nodes[0], make_list(nodes[1:]))
+
+    def lists_are_equal(l1: ListNode | None, l2: ListNode | None) -> bool:
+        """Compare two lists"""
+        while l1 and l2:
+            if l1.val != l2.val:
+                return False
+            l1 = getattr(l1, "next", None)
+            l2 = getattr(l2, "next", None)
+        return l1 is None and l2 is None
+
+    def pass_not_pass(l1: ListNode | None, l2: ListNode | None) -> str:
+        """Wrapper for lists_are_equal() for testing output"""
+        if lists_are_equal(l1, l2):
+            return "Pass"
+        return "NOT PASSING"
+
+    # Test cases, each sublist is [head, n, expected]
+    cases = {
+        1: [[1, 2, 3, 4, 5], 2, [1, 2, 3, 5]],
+        2: [[1], 1, []],
+        3: [[1, 2], 1, [1]],
+    }
+    solution = Solution()
+
+    round_times = []
+    for i, c in enumerate(cases.values()):
+        h_list = make_list(c[0])
+        n_value = c[1]
+        expect = make_list(c[2])
+        start = time.perf_counter()
+        result = solution.removeNthFromEnd(h_list, n_value)
+        end = time.perf_counter()
+        round_times.append(end - start)
+        print(f"Test {i}:", pass_not_pass(result, expect))
+
+        # i1 = get_listnode_as_str(l1_test)
+        # i2 = get_listnode_as_str(l2_test)
+        # print(f"{i1=}\n{i2=}")
+
+        # r = get_listnode_as_str(result)
+        # e = get_listnode_as_str(expect)
+        # print(f"{r=}\n{e=}\n")
+
+    print(f"Total time: {sum(round_times)} seconds\n")
